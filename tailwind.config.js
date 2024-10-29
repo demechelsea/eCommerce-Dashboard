1d08926e24eaa0714ee3104fe2bf79f07d6b8d86
Dame <demechelsea@gmail.com>
2024-10-29 10:59:13 +0100

    some sidebar theme changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,20 +6,7 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        bodydark2: 'rgba(138, 153, 175, 1)',
-        'meta-4': {
-          DEFAULT: 'rgba(255, 255, 255, 0.1)', 
-          dark: 'rgba(40, 44, 52, 1)', 
-        },
-      },
-      fontSize: {
-        sm: '0.875rem',
-      },
-      fontWeight: {
-        medium: 500,
-      },
-      width: {
-        '125': '500px', 
+       
       },
     },
   },
