dae10cb2c311e68a1f308fe9620bb618bd5372a4
iphadan <ephadaniel177@gmail.com>
2024-10-30 16:17:09 +0300

    select under ui-elements added

--- src/app/sidebar/sidebar.component.ts
+++ src/app/sidebar/sidebar.component.ts
@@ -43,93 +43,98 @@ export class SidebarComponent {
 
   }
   accordion() {
+    this.subMenuSelected = "accordion"
 
     this.router.navigate(['/accordion'])
+
   }
   alerts() {
 // this.toggleMenu("alert")
+this.subMenuSelected = "alerts";
     this.router.navigate(['/alerts']);
   }
   avatars() {
-
+    this.subMenuSelected = "avatars";
     this.router.navigate(['/avatars'])
   }
   badge() {
+    this.subMenuSelected = "badge";
     this.router.navigate(['/badge'])
   }
 
 breadcump() {
+  this.subMenuSelected = "breadcrumb";
     this.router.navigate(['/breadcrumb'])
   }
 
   buttons() {
-
+    this.subMenuSelected = "buttons";
     this.router.navigate(['/buttons'])
   }
 
   groupButtons() {
-
+    this.subMenuSelected = "buttons-group";
     this.router.navigate(['/buttons-group'])
   }
 
   cards() {
-
+    this.subMenuSelected = "cards";
     this.router.navigate(['/cards'])
   }
   carousel() {
-
+    this.subMenuSelected = "carousel";
     this.router.navigate(['/carousel'])
   }
 
   dropdowns() {
-
+    this.subMenuSelected = "dropdowns";
     this.router.navigate(['/dropdowns'])
   }
 
   images() {
-
+    this.subMenuSelected = "images"
     this.router.navigate(['/images'])
   }
   list() {
-
+    this.subMenuSelected = "list";
     this.router.navigate(['/list'])
   }
   modals() {
-
+    this.subMenuSelected = "modals"
     this.router.navigate(['/modals'])
   }
 
 
   notifications() {
-
+    this.subMenuSelected = "notifications"
     this.router.navigate(['/notifications'])
   }
   pagination() {
-
+    this.subMenuSelected = "pagination"
     this.router.navigate(['/pagination'])
   }
   popovers() {
-
+    this.subMenuSelected = "popovers"
     this.router.navigate(['/popovers'])
   }
   progress() {
-
+    this.subMenuSelected = "progress"
     this.router.navigate(['/progress'])
   }
   spinners() {
-
+    this.subMenuSelected = "spinners"
     this.router.navigate(['/spinners'])
   }
   tabs() {
-
+    this.subMenuSelected = "tabs"
     this.router.navigate(['/tabs'])
   }
   tooltips() {
-
+    this.subMenuSelected = "tooltips"
     this.router.navigate(['/tooltips'])
   }
   videos() {
-
+    this.subMenuSelected = "videos"
     this.router.navigate(['/videos'])
   }
 
