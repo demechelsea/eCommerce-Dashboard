0faee962d762d6d9b36e061b8f3cb81f2829c5d4
iphadan <ephadaniel177@gmail.com>
2024-10-30 22:34:07 +0300

    images added upto images component

--- src/app/ui-elements/drop-downs/drop-downs.component.ts
+++ src/app/ui-elements/drop-downs/drop-downs.component.ts
@@ -1,12 +1,19 @@
-import { Component } from '@angular/core';
+import { CommonModule, NgClass } from '@angular/common';
+import { Component, NgModule } from '@angular/core';
 
 @Component({
   selector: 'app-drop-downs',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './drop-downs.component.html',
   styleUrl: './drop-downs.component.scss'
 })
 export class DropDownsComponent {
+  // instead of declaring the following vars, look if there is a way to add property style="display:none" based on a condition
+openDropDown_1: boolean = false;
+openDropDown_2: boolean = false;
+openDropDown_3: boolean = false;
+// rotaion not working
+
 
 }
--- src/app/ui-elements/images/images.component.ts
+++ src/app/ui-elements/images/images.component.ts
@@ -8,5 +8,5 @@ import { Component } from '@angular/core';
   styleUrl: './images.component.scss'
 })
 export class ImagesComponent {
-
+// theme problem  
 }
