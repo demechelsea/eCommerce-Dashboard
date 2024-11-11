6b5da3591a387f8e34783e851c7971eb308bda70
iphadan <ephadaniel177@gmail.com>
2024-11-11 16:18:56 +0300

    avatar alert breadcrumb

--- src/app/ui-elements/breadcump/breadcump.component.ts
+++ src/app/ui-elements/breadcump/breadcump.component.ts
@@ -1,9 +1,18 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-breadcump',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule,
+    MatCardModule,
+    MatIconModule,
+    RouterModule
+  ],
   templateUrl: './breadcump.component.html',
   styleUrl: './breadcump.component.scss'
 })
