84c7ffe55430dada23a503e25d1ffd299d51b875
Dame <demechelsea@gmail.com>
2024-11-18 11:07:20 +0100

    trying to apply angular theme

--- src/app/ui-elements/buttons/buttons.component.ts
+++ src/app/ui-elements/buttons/buttons.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-buttons',
   standalone: true,
-  imports: [],
+  imports: [MatButtonModule],
   templateUrl: './buttons.component.html',
   styleUrl: './buttons.component.scss'
 })
