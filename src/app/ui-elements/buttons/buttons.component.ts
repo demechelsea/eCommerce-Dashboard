276e999cd3127f53f415bf5a6d9cd07240e5df1b
Dame <demechelsea@gmail.com>
2024-11-25 11:41:09 +0100

    the button component is changed from raw html tags into angular material element

--- src/app/ui-elements/buttons/buttons.component.ts
+++ src/app/ui-elements/buttons/buttons.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-buttons',
   standalone: true,
-  imports: [MatButtonModule],
+  imports: [MatButtonModule, MatIconModule],
   templateUrl: './buttons.component.html',
   styleUrl: './buttons.component.scss'
 })
