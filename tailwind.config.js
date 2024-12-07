cbaa74ffc047d8f7cee38b4bd5c1dbe090dfa2c7
Dame <demechelsea@gmail.com>
2024-12-07 22:50:42 +0100

    the button is working now according to the new requirement

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,18 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-       
+        primary: {
+          DEFAULT: '#3b50df', // Primary color
+          hover: '#3347c5',  // Hover state
+        },
+        secondary: {
+          DEFAULT: '#10b982', // Secondary color
+          hover: '#0e966f',  // Hover state
+        },
+        tertiary: {
+          DEFAULT: '#1c2333', // Tertiary color (black)
+          hover: '#151a26',  // Hover state
+        },
       },
     },
   },
