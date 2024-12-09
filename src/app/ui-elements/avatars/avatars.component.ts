df4284268758134f967a3affd812190bd66f0bb3
Dame <demechelsea@gmail.com>
2024-12-09 16:42:51 +0100

    buttons-group is updated

--- src/app/ui-elements/avatars/avatars.component.ts
+++ src/app/ui-elements/avatars/avatars.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { MatCard, MatCardContent } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-avatars',
   standalone: true,
-  imports: [MatCard,MatCardContent],
+  imports: [MatCard,MatCardContent, MatIconModule],
   templateUrl: './avatars.component.html',
   styleUrl: './avatars.component.scss'
 })
