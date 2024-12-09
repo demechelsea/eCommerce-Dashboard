cbdb14eb598bd03d814747ce16adf4f4ec377249
Dame <demechelsea@gmail.com>
2024-12-09 16:00:53 +0100

    buttons-group is done with the new requirement

--- src/app/ui-elements/buttons-group/buttons-group.component.ts
+++ src/app/ui-elements/buttons-group/buttons-group.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-buttons-group',
   standalone: true,
-  imports: [],
+  imports: [MatButtonModule, MatIconModule],
   templateUrl: './buttons-group.component.html',
   styleUrl: './buttons-group.component.scss'
 })
