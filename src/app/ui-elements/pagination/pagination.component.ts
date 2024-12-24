c486f0fe3237e8d79d7f9d6009afe46214f65362
Dame <demechelsea@gmail.com>
2024-12-24 10:07:58 +0100

    badge, buttons and buttons-group are completed

--- src/app/ui-elements/pagination/pagination.component.ts
+++ src/app/ui-elements/pagination/pagination.component.ts
@@ -1,12 +1,19 @@
 import { Component } from '@angular/core';
+import { MatPaginator, PageEvent } from '@angular/material/paginator';
 
 @Component({
   selector: 'app-pagination',
   standalone: true,
-  imports: [],
+  imports: [MatPaginator],
   templateUrl: './pagination.component.html',
   styleUrl: './pagination.component.scss'
 })
 export class PaginationComponent {
+  totalItems = 100; // Total number of items
+  pageSize = 10; // Default page size
 
+  onPageChange(event: PageEvent) {
+    console.log('Page Event:', event);
+    // Handle pagination logic (e.g., fetch new data based on pageIndex)
+  }
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,4 @@
-module.exports = { 
+module.exports = {
   content: [
     "./src/**/*.{html,ts}",
   ],
@@ -7,15 +7,39 @@ module.exports = {
       colors: {
         primary: {
           DEFAULT: '#3b50df', // Primary color
-          hover: '#3347c5',  // Hover state
+          hover: '#3347c5',   // Hover state
         },
         secondary: {
           DEFAULT: '#10b982', // Secondary color
-          hover: '#0e966f',  // Hover state
+          hover: '#0e966f',   // Hover state
         },
         tertiary: {
           DEFAULT: '#1c2333', // Tertiary color (black)
-          hover: '#151a26',  // Hover state
+          hover: '#151a26',   // Hover state
+        },
+        gray: {
+          DEFAULT: '#637381', // Ensure it overrides other styles
+          hover: '#4f5c65',
+        },
+        light: {
+          DEFAULT: '#EFEFEF', // Light color
+          hover: '#e0e0e0',   // Hover state
+        },
+        warning: {
+          DEFAULT: '#F9C107', // Warning color
+          hover: '#dba406',   // Hover state
+        },
+        danger: {
+          DEFAULT: '#DC3545', // Danger color
+          hover: '#c12d3b',   // Hover state
+        },
+        success: {
+          DEFAULT: '#3CA745', // Success color
+          hover: '#33903b',   // Hover state
+        },
+        info: {
+          DEFAULT: '#3BA2B8', // Info color
+          hover: '#3290a3',   // Hover state
         },
       },
     },
@@ -31,6 +55,7 @@ module.exports = {
         '.no-scrollbar::-webkit-scrollbar': {
           display: 'none',               // Chrome, Safari, and Edge
         },
+        
       });
     },
   ],
